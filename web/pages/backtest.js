97f907b278f5293aa9063d336ad49c8bdce91793
Hali-creater <hali.scenarios@gmail.com>
2025-08-09 16:45:52 +0500

    Create backtest.js

---
+++ web/pages/backtest.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Backtest = () => {
+    return (
+        <div>
+            <Header />
+            <h2>Backtest Strategies</h2>
+            <p>Coming soon!</p>
+            <Footer />
+        </div>
+    );
+};
+
+export default Backtest;
